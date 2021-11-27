689bb38045b31c31ed263ff5cde325c40e6e0c29
Robin Angel Romero Cordero <ingrobinromeo@gmail.com>
2021-11-27 12:48:04 -0500

    practica done, arrancar proyecto crm

--- db/schema.js
+++ db/schema.js
@@ -2,20 +2,7 @@ const { gql } = require("apollo-server");
 
 // schema
 const typeDefs = gql`
-  type Curso {
-    titulo: String
-    tecnologia: String
-  }
-  type Tecnologia {
-    tecnologia: String
-  }
-  input CursoInput {
-    tecnologia: String
-  }
-  type Query {
-    obtenerCursos(input: CursoInput!): [Curso]
-    obtenerTecnologias: [Tecnologia]
-  }
+
 `;
 
 module.exports = typeDefs;
--- index.js
+++ index.js
@@ -11,12 +11,7 @@ const resolvers = require('./db/resolvers')
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: () => { 
-    const miContext = "Hola"
-    return {
-      miContext
-    }
-  },
+  
   // el siguiente plugin es para cambiar el landing page a graphql playground
   plugins: [
     ApolloServerPluginLandingPageGraphQLPlayground({
