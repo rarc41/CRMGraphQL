a9a4128248ac0452859dcfc89d95cfded2cc8a95
Robin Angel Romero Cordero <ingrobinromeo@gmail.com>
2021-11-27 14:44:57 -0500

    crendo type de usuario

--- db/resolvers.js
+++ db/resolvers.js
@@ -4,6 +4,9 @@ const resolvers = {
   Query: {
     obtenerCurso: () => 'Curso GraphQL'
   },
+  Mutation: {
+    crearUsuario: () => 'Usuario creado'
+  }
 };
 
 module.exports = resolvers;
--- db/schema.js
+++ db/schema.js
@@ -2,8 +2,20 @@ const { gql } = require("apollo-server");
 
 // schema
 const typeDefs = gql`
+  type Usuario {
+    id: ID!
+    nombre: String!
+    apellido: String!
+    email: String!
+    creado: String!
+  }
+
   type Query {
     obtenerCurso: String
+  }
+
+  type Mutation {
+    crearUsuario: String
   }
 `;
 
